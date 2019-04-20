225de4179614f17f2650d1077a22bfe947b5eb14
fabiopriori <ale-erromanista@hotmail.it>
2019-04-20 13:04:48 +0000

    Creazione progetto maven

---
+++ compiti-informatica/src/main/java/com/fabio/compiti/App.java
@@ -0,0 +1,13 @@
+package com.fabio.compiti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
